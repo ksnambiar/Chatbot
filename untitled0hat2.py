7cc0af58d60125ca96d8a844aca3d70f5e587527
NambiarSidharth <32272949+NambiarSidharth@users.noreply.github.com>
2019-04-16 09:29:38 +0530

    finished chatbot with pytorch

--- untitled0hat2.py
+++ untitled0hat2.py
@@ -1,163 +1,175 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Feb 25 21:59:31 2019
+Created on Sun Mar 24 07:26:28 2019
 
 @author: Sidharth
 """
 
-#building chatbot with deep nlp
+# Building a ChatBot with Deep NLP
 
+
+
+# Importing the libraries
 import numpy as np
 import tensorflow as tf
 import re
 import time
 
-####################DATA PRE-PROCESSING##############################3
-lines = open("movie_lines.txt",encoding="utf-8", errors = 'ignore').read().split('\n')
-conversations = open("movie_conversations.txt",encoding="utf-8", errors = 'ignore').read().split('\n')
 
-id2lines= {}
-# creating a key value pair for ll dialogues
+
+########## PART 1 - DATA PREPROCESSING ##########
+
+
+
+# Importing the dataset
+lines = open('movie_lines.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
+conversations = open('movie_conversations.txt', encoding = 'utf-8', errors = 'ignore').read().split('\n')
+
+# Creating a dictionary that maps each line and its id
+id2line = {}
 for line in lines:
-    _line = line.split(" +++$+++ ")
-    if len(_line)==5:
-        id2lines[_line[0]]=_line[4]
+    _line = line.split(' +++$+++ ')
+    if len(_line) == 5:
+        id2line[_line[0]] = _line[4]
 
-# creating a list for all conversations
-conversations_ids=[]
+# Creating a list of all of the conversations
+conversations_ids = []
 for conversation in conversations[:-1]:
-    _conversation = conversation.split(" +++$+++ ")[-1][1:-1].replace("'","").replace(" ","")
-    conversations_ids.append(_conversation.split(","))
+    _conversation = conversation.split(' +++$+++ ')[-1][1:-1].replace("'", "").replace(" ", "")
+    conversations_ids.append(_conversation.split(','))
 
-#creating new lists for questions and answers
-questions=[]
-answers=[]
+# Getting separately the questions and the answers
+questions = []
+answers = []
 for conversation in conversations_ids:
-    for i in range(len(conversation)-1):
-        questions.append(id2lines[conversation[i]])
-        answers.append(id2lines[conversation[i+1]])
+    for i in range(len(conversation) - 1):
+        questions.append(id2line[conversation[i]])
+        answers.append(id2line[conversation[i+1]])
 
-#first cleaning
+# Doing a first cleaning of the texts
 def clean_text(text):
     text = text.lower()
-    text = re.sub(r"i'm","i am",text)
-    text = re.sub(r"he's","he is",text)
-    text = re.sub(r"she's","she is",text)
-    text = re.sub(r"that's","that is",text)
-    text = re.sub(r"what's","what is",text)
-    text = re.sub(r"where's","where is",text)
-    text = re.sub(r"it's","it is",text)
-    text = re.sub(r"\'ll","will",text)
-    text = re.sub(r"\'ve","have",text)
-    text = re.sub(r"'re","are",text)
-    text = re.sub(r"'d","are",text)
-    text = re.sub(r"won't","will not",text)
-    text = re.sub(r"can't","can not",text)
-    text = re.sub(r"[-()\"#/@;:<>{}+=~|.?,]","",text)
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"can't", "cannot", text)
+    text = re.sub(r"[-()\"#/@;:<>{}+=~|.?,]", "", text)
     return text
 
-#cleaning the questions
+# Cleaning the questions
 clean_questions = []
 for question in questions:
     clean_questions.append(clean_text(question))
 
-#cleaning the answers
+# Cleaning the answers
 clean_answers = []
 for answer in answers:
     clean_answers.append(clean_text(answer))
-    
-#establishing the frequency of the word occurences
-word2count={}
 
+# Creating a dictionary that maps each word to its number of occurrences
+word2count = {}
 for question in clean_questions:
     for word in question.split():
         if word not in word2count:
-            word2count[word]=1
+            word2count[word] = 1
         else:
-            word2count[word]+=1
-
+            word2count[word] += 1
 for answer in clean_answers:
     for word in answer.split():
         if word not in word2count:
-            word2count[word]=1
+            word2count[word] = 1
         else:
-            word2count[word]+=1
-#filtering words
-#ceck later
-threshhold=20
-questionswords2int={}
-word_number=0
-for word,count in word2count.items():
-    if count>threshhold:
-        questionswords2int[word]=count
-        count+=1
+            word2count[word] += 1
 
-answerswords2int = {}
-word_number=0
+# Creating two dictionaries that map the questions words and the answers words to a unique integer
+threshold_questions = 20
+questionswords2int = {}
+word_number = 0
 for word, count in word2count.items():
-    if count>=threshhold:
-         answerswords2int[word]=count
-         count+=1
-            
-##putting special keys
-tokens=['<PAD>','<EOS>','<OUT>','<SOS>']
-for token in tokens:
-    questionswords2int[token]=len(questionswords2int)+1
+    if count >= threshold_questions:
+        questionswords2int[word] = word_number
+        word_number += 1
+threshold_answers = 20
+answerswords2int = {}
+word_number = 0
+for word, count in word2count.items():
+    if count >= threshold_answers:
+        answerswords2int[word] = word_number
+        word_number += 1
 
+# Adding the last tokens to these two dictionaries
+tokens = ['<PAD>', '<EOS>', '<OUT>', '<SOS>']
 for token in tokens:
-    answerswords2int[token]=len(answerswords2int)+1
-    
-#creating inverse dictionary of the answerswords2int dictionary
-answersints2word = {w_i:w for w,w_i in answerswords2int.items()}
+    questionswords2int[token] = len(questionswords2int) + 1
+for token in tokens:
+    answerswords2int[token] = len(answerswords2int) + 1
 
-#appending eos to every line
+# Creating the inverse dictionary of the answerswords2int dictionary
+answersints2word = {w_i: w for w, w_i in answerswords2int.items()}
+
+# Adding the End Of String token to the end of every answer
 for i in range(len(clean_answers)):
     clean_answers[i] += ' <EOS>'
 
-##replacing all words with integers
-questions_into_int=[]
+# Translating all the questions and the answers into integers
+# and Replacing all the words that were filtered out by <OUT> 
+questions_into_int = []
 for question in clean_questions:
-    ints=[]
+    ints = []
     for word in question.split():
         if word not in questionswords2int:
             ints.append(questionswords2int['<OUT>'])
         else:
             ints.append(questionswords2int[word])
     questions_into_int.append(ints)
-
-answers_to_int = []
+answers_into_int = []
 for answer in clean_answers:
     ints = []
-    for word in question.split():
-        if word not in questionswords2int:
+    for word in answer.split():
+        if word not in answerswords2int:
             ints.append(answerswords2int['<OUT>'])
         else:
             ints.append(answerswords2int[word])
-    answers_to_int.append(ints)
+    answers_into_int.append(ints)
 
-#sorting questions and answers by the length of questions
-sorted_clean_questions=[]
-sorted_clean_answers=[]
-for length in range(1,25+1):
+# Sorting questions and answers by the length of questions
+sorted_clean_questions = []
+sorted_clean_answers = []
+for length in range(1, 25 + 1):
     for i in enumerate(questions_into_int):
-        if len(i[1])==length:
+        if len(i[1]) == length:
             sorted_clean_questions.append(questions_into_int[i[0]])
-            sorted_clean_answers.append(answers_to_int[i[0]])
+            sorted_clean_answers.append(answers_into_int[i[0]])
 
-########################## PART 2 - seq-seq models ###################33
+
+
+########## PART 2 - BUILDING THE SEQ2SEQ MODEL ##########
+
+
+
+# Creating placeholders for the inputs and the targets
 def model_inputs():
-    inputs = tf.placeholder(tf.int32,[None,None],name="input")
-    targets = tf.placeholder(tf.int32,[None,None],name="target")
-    lr = tf.placeholder(tf.float32,name="learning_rate")
-    keep_prob= tf.placeholder(tf.float32,name="keep_prob");
-    return inputs,targets,lr,keep_prob
+    inputs = tf.placeholder(tf.int32, [None, None], name = 'input')
+    targets = tf.placeholder(tf.int32, [None, None], name = 'target')
+    lr = tf.placeholder(tf.float32, name = 'learning_rate')
+    keep_prob = tf.placeholder(tf.float32, name = 'keep_prob')
+    return inputs, targets, lr, keep_prob
 
-#preprocess the tergets
-def preprocess_targets(targets,word2int,batch_size):
-    left_side=tf.fill([batch_size,1],word2int['<SOS>'])
-    right_side = tf.strided_slice(targets,[0,0],[batch_size,-1],[1,1])
-    preprocessed_targets = tf.concat(left_side, right_side, 1)
+# Preprocessing the targets
+def preprocess_targets(targets, word2int, batch_size):
+    left_side = tf.fill([batch_size, 1], word2int['<SOS>'])
+    right_side = tf.strided_slice(targets, [0,0], [batch_size, -1], [1,1])
+    preprocessed_targets = tf.concat([left_side, right_side], 1)
     return preprocessed_targets
+
 # Creating the Encoder RNN
 def encoder_rnn(rnn_inputs, rnn_size, num_layers, keep_prob, sequence_length):
     lstm = tf.contrib.rnn.BasicLSTMCell(rnn_size)
@@ -173,7 +185,7 @@ def encoder_rnn(rnn_inputs, rnn_size, num_layers, keep_prob, sequence_length):
 # Decoding the training set
 def decode_training_set(encoder_state, decoder_cell, decoder_embedded_input, sequence_length, decoding_scope, output_function, keep_prob, batch_size):
     attention_states = tf.zeros([batch_size, 1, decoder_cell.output_size])
-    attention_keys, attention_values, attention_score_function, attention_construct_function = tf.contrib.seq2seq.prepare_attention(attention_states, attention_option = "bahdanau", num_units = decoder_cell.output_size)
+    attention_keys, attention_values, attention_score_function, attention_construct_function = tf.contrib.seq2seq.DynamicAttentionWrapper(attention_states, attention_option = "bahdanau", num_units = decoder_cell.output_size)
     training_decoder_function = tf.contrib.seq2seq.attention_decoder_fn_train(encoder_state[0],
                                                                               attention_keys,
                                                                               attention_values,
@@ -191,7 +203,7 @@ def decode_training_set(encoder_state, decoder_cell, decoder_embedded_input, seq
 # Decoding the test/validation set
 def decode_test_set(encoder_state, decoder_cell, decoder_embeddings_matrix, sos_id, eos_id, maximum_length, num_words, decoding_scope, output_function, keep_prob, batch_size):
     attention_states = tf.zeros([batch_size, 1, decoder_cell.output_size])
-    attention_keys, attention_values, attention_score_function, attention_construct_function = tf.contrib.seq2seq.prepare_attention(attention_states, attention_option = "bahdanau", num_units = decoder_cell.output_size)
+    attention_keys, attention_values, attention_score_function, attention_construct_function = tf.contrib.seq2seq.DynamicAttentionWrapper(attention_states, attention_option = "bahdanau", num_units = decoder_cell.output_size)
     test_decoder_function = tf.contrib.seq2seq.attention_decoder_fn_inference(output_function,
                                                                               encoder_state[0],
                                                                               attention_keys,
@@ -269,8 +281,8 @@ def seq2seq_model(inputs, targets, keep_prob, batch_size, sequence_length, answe
 
 
 
-###################################training the model#############################
-    
+########## PART 3 - TRAINING THE SEQ2SEQ MODEL ##########
+
 
 
 # Setting the Hyperparameters
